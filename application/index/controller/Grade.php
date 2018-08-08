36a8f074f4d40e57a568cf8e5162e3dcda4e664f
Hider Wang <724598894@qq.com>
2018-08-08 19:15:33 +0800

    班级管理-班级的增删查改完成

--- application/index/controller/Grade.php
+++ application/index/controller/Grade.php
@@ -17,12 +17,14 @@ class Grade extends Controller
         $this->db = new \app\index\model\Grade();
     }
 
+    //班级列表首页
     public function index()
     {
         $gradeList = db('grade')->select();
-        $count = count($this->db);
-        $this->assign('count', $count);
+//        $count = count($this->db);
+        $count = \app\index\model\Grade::count();
         $this->assign('gradeList', $gradeList);
+        $this->assign('count', $count);
         return $this->fetch();
     }
 
@@ -43,6 +45,7 @@ class Grade extends Controller
         }
     }
 
+    //班级编辑首页
     public function edit(Request $request)
     {
         //获取到要编辑的班级ID
@@ -56,14 +59,78 @@ class Grade extends Controller
 
         //给当前页面seo变量赋值
         $this->assign('title', '编辑班级');
-        $this->assign('keywords', 'php.cn');
-        $this->assign('desc', 'PHP中文网ThinkPHP5开发实战课程');
 
         //给当前编辑模板赋值
         $this->assign('grade_info', $result);
 
         //渲染编辑模板
         return $this->fetch('edit');
+    }
+
+    //班级编辑
+    public function doEdit(Request $request)
+    {
+        //从提交表单中排除关联字段teacher字段
+//        $data = $request -> except('teacher');
+        $data = $request->param();  //如果全部获取,提交会提示缺少字段teacher
+
+        //设置更新条件
+        $condition = ['id' => $data['id']];
+
+        //更新当前记录
+        $result = \app\index\model\Grade::update($data, $condition);
+
+        //设置返回数据
+        $status = 0;
+        $message = '更新失败,请检查';
+
+        //检测更新结果,将结果返回给grade_edit模板中的ajax提交回调处理
+        if (true == $result) {
+            $status = 1;
+            $message = '恭喜, 更新成功~~';
+        }
+        return ['status' => $status, 'message' => $message];
+    }
+
+    //班级添加界面
+    public function add()
+    {
+        //给模板赋值seo变量
+        $this->assign('title', '添加班级');
+
+        //渲染添加模板
+        return $this->fetch();
+    }
+
+    //添加班级
+    public function doAdd(Request $request)
+    {
+        //从提交表单中获取数据
+        $data = $request->param();
+
+        //更新当前记录
+        $result = \app\index\model\Grade::create($data);
+
+        //设置返回数据的初始值
+        $status = 0;
+        $message = '添加失败,请检查';
+
+        //检测更新结果,将结果返回给grade_edit模板中的ajax提交回调处理
+        if (true == $result) {
+            $status = 1;
+            $message = '恭喜, 添加成功~~';
+        }
+
+        //自动转为json格式返回
+        return ['status' => $status, 'message' => $message];
+    }
+
+    //班级删除
+    public function delete(Request $request)
+    {
+        $user_id = $request->param('id');
+        \app\index\model\Grade::destroy($user_id);
+
     }
 
 }
--- application/index/model/Grade.php
+++ application/index/model/Grade.php
@@ -11,13 +11,10 @@ class Grade extends Model
 {
     protected $pk = 'id';
     protected $table = 'grade';
-    use SoftDelete;
 
     //设置当前表默认日期时间显示格式
     protected $dateFormat = 'Y/m/d';
 
-    //定义表中的删除时间字段,来记录删除时间
-    protected $deleteTime = 'delete_time';
 
     // 开启自动写入时间戳
     protected $autoWriteTimestamp = true;
