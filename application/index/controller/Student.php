791ae9070c47955393f5893f1a914d3e89f4986a
Hider Wang <724598894@qq.com>
2018-08-10 14:58:03 +0800

    学生管理-学生状态变更

--- application/index/controller/Student.php
+++ application/index/controller/Student.php
@@ -3,6 +3,7 @@
 namespace app\index\controller;
 
 use think\Controller;
+use think\Request;
 
 class Student extends Controller
 {
@@ -20,9 +21,23 @@ class Student extends Controller
             $value->grade = $value->grade->name;
         }
 
+        $this->assign('title', '学生列表');
         $this->assign('studentList', $studentList);
         $this->assign('count', $count);
 
         return $this->fetch();
+    }
+
+    //学生状态变更
+    public function setStatus(Request $request)
+    {
+        $student_id = $request->param('id');
+        $result = \app\index\model\Student::get($student_id);
+
+        if ($result->getData('status') == 1) {
+            \app\index\model\Student::update(['status' => 0], ['id' => $student_id]);
+        } else {
+            \app\index\model\Student::update(['status' => 1], ['id' => $student_id]);
+        }
     }
 }
